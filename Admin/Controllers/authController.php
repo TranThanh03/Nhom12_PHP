fa2b12d224f1d70e5ebaee68df5245101177fe52
Thành Trần <107708016+TranThanh03@users.noreply.github.com>
2024-12-01 14:25:48 +0700

    Update authController.php

--- Admin/Controllers/authController.php
+++ Admin/Controllers/authController.php
@@ -3,13 +3,13 @@
         
         public function index() {
             if(empty($_SESSION['accountAdmin']) && empty($_SESSION['passwordAdmin'])) {
-                return header('location: /WebsiteSaoViet/index.php?controller=home&action=index' );
+                return header('location: /Nhom12_PHP/index.php?controller=home&action=index' );
             }
         }
 
         public function logout() {
             unset($_SESSION['accountAdmin']);
             unset($_SESSION['passwordAdmin']);
-            header('location: /WebsiteSaoViet/index.php?controller=home&action=index');
+            header('location: /Nhom12_PHP/index.php?controller=home&action=index');
         }
     }
